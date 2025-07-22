b58734161243b35c8ceedae7eb684c2164b82812
Mahani Kunche <k.mahani466@gmail.com>
2025-07-22 23:10:56 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+import AddEvent from "./pages/AddEvent";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/add-event" element={<AddEvent />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
